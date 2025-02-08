0983e659138253fecd324e4e83cbee5008d07bcc
cousinbigcountry <fountainkylec.gmail.com>
2025-02-08 18:51:22 -0500

    projects page built, need to make responsive and add other projects

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,7 +1,154 @@
 import React from "react";
+import { IoLogoJavascript } from "react-icons/io";
+import { MdCss } from "react-icons/md";
+import { MdHtml } from "react-icons/md";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section>
+      <br />
+      <br />
+      <div className="flex justify-center mb-5">
+        <h1 className="text-3xl">Front-end Projects</h1>
+      </div>
+      <div className="grid grid-cols-3 ml-32">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-black">
+          <img className="w-full" src="Tpp-website.png" alt="Tpp website" />
+          <div className="px-6 py-4 text-center">
+            <p className="font-bold text-xl mb-2">
+              <a className="hover:text-[#7791d6]" href="https://tylerspub.com/">
+                Tyler Pub at The Pier
+              </a>
+            </p>
+          </div>
+          <div className="flex justify-evenly">
+            <span>
+              <MdHtml size={42} />
+            </span>
+            <span>
+              <MdCss size={42} />
+            </span>
+            <span>
+              <IoLogoJavascript size={35} />
+            </span>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-black">
+          <img className="w-full" src="Tpp-website.png" alt="Tpp website" />
+          <div className="px-6 py-4 text-center">
+            <p className="font-bold text-xl mb-2">
+              <a className="hover:text-[#7791d6]" href="https://tylerspub.com/">
+                Tyler Pub at The Pier
+              </a>
+            </p>
+          </div>
+          <div className="flex justify-evenly">
+            <span>
+              <MdHtml size={42} />
+            </span>
+            <span>
+              <MdCss size={42} />
+            </span>
+            <span>
+              <IoLogoJavascript size={35} />
+            </span>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-black">
+          <img className="w-full" src="Tpp-website.png" alt="Tpp website" />
+          <div className="px-6 py-4 text-center">
+            <p className="font-bold text-xl mb-2">
+              <a className="hover:text-[#7791d6]" href="https://tylerspub.com/">
+                Tyler Pub at The Pier
+              </a>
+            </p>
+          </div>
+          <div className="flex justify-evenly">
+            <span>
+              <MdHtml size={42} />
+            </span>
+            <span>
+              <MdCss size={42} />
+            </span>
+            <span>
+              <IoLogoJavascript size={35} />
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <div className="flex justify-center mb-5">
+        <h1 className="text-3xl">Backend Projects</h1>
+      </div>
+      <div className="grid grid-cols-3 ml-32">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-black">
+          <img className="w-full" src="Tpp-website.png" alt="Tpp website" />
+          <div className="px-6 py-4 text-center">
+            <p className="font-bold text-xl mb-2">
+              <a className="hover:text-[#7791d6]" href="https://tylerspub.com/">
+                Tyler Pub at The Pier
+              </a>
+            </p>
+          </div>
+          <div className="flex justify-evenly">
+            <span>
+              <MdHtml size={42} />
+            </span>
+            <span>
+              <MdCss size={42} />
+            </span>
+            <span>
+              <IoLogoJavascript size={35} />
+            </span>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-black">
+          <img className="w-full" src="Tpp-website.png" alt="Tpp website" />
+          <div className="px-6 py-4 text-center">
+            <p className="font-bold text-xl mb-2">
+              <a className="hover:text-[#7791d6]" href="https://tylerspub.com/">
+                Tyler Pub at The Pier
+              </a>
+            </p>
+          </div>
+          <div className="flex justify-evenly">
+            <span>
+              <MdHtml size={42} />
+            </span>
+            <span>
+              <MdCss size={42} />
+            </span>
+            <span>
+              <IoLogoJavascript size={35} />
+            </span>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-black">
+          <img className="w-full" src="Tpp-website.png" alt="Tpp website" />
+          <div className="px-6 py-4 text-center">
+            <p className="font-bold text-xl mb-2">
+              <a className="hover:text-[#7791d6]" href="https://tylerspub.com/">
+                Tyler Pub at The Pier
+              </a>
+            </p>
+          </div>
+          <div className="flex justify-evenly">
+            <span>
+              <MdHtml size={42} />
+            </span>
+            <span>
+              <MdCss size={42} />
+            </span>
+            <span>
+              <IoLogoJavascript size={35} />
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
